7515f17f314439b17d679d8366052556fa892492
CyberPanda2006 <109407866+CyberPanda2006@users.noreply.github.com>
2022-07-16 12:29:53 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,8 +19,8 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
+global.owner = ['+94782892785']
+global.premium = ['+94782892785']
 global.ownernomer = 'Your Number'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
